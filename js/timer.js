c620dc59655427e05cc261153b21373f88f17e7c
CamdzicAlden <aldencamdzic3@gmail.com>
2025-08-28 21:35:51 +0200

    feat: Make first timer ticking script

--- js/timer.js
+++ js/timer.js
@@ -6,4 +6,26 @@ stopwatch.onmouseover = () => stopwatch.src = "./icons/StopwatchWhiteFill.svg"
 stopwatch.onmouseleave = () => stopwatch.src = "./icons/StopwatchWhiteEmpty.svg"
 
 
+const circle = document.querySelector(".progress");
+const text = document.getElementById("timeText");
+const radius = circle.r.baseVal.value;
+const circumference = 2 * Math.PI * radius;
+
+circle.style.strokeDasharray = circumference;
+circle.style.strokeDashoffset = 0;
+
+let time = 30;
+let current = time;
+
+function updateTimer(){
+    current--;
+    text.textContent = current;
+
+    const offset = circumference - (current/time) * circumference;
+    circle.style.strokeDashoffset = offset;
+
+    if(current <= 0) clearInterval(timer);
+}
+
+const timer = setInterval(updateTimer, 1000);
 
