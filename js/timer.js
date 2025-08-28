75c1766c3e3416ba2eb63ea3b79359c530ac6c6d
CamdzicAlden <aldencamdzic3@gmail.com>
2025-08-28 21:15:30 +0200

    feat: Make timer svg circles

--- js/timer.js
+++ js/timer.js
@@ -4,3 +4,6 @@ stopwatch = document.getElementById("stopwatch");
 
 stopwatch.onmouseover = () => stopwatch.src = "./icons/StopwatchWhiteFill.svg"
 stopwatch.onmouseleave = () => stopwatch.src = "./icons/StopwatchWhiteEmpty.svg"
+
+
+
