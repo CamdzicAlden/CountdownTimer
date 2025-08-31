f313e4262ec749ab07afb24db472e9d4b85985a8
CamdzicAlden <aldencamdzic3@gmail.com>
2025-08-31 12:26:31 +0200

    feat: Make logic for hours, minutes and seconds in timer and comment everything

--- js/timer.js
+++ js/timer.js
@@ -1,59 +1,87 @@
-//Code for stopwatch icon hovering
-let stopwatch;
-stopwatch = document.getElementById("stopwatch");
-
-stopwatch.onmouseover = () => stopwatch.src = "./icons/StopwatchWhiteFill.svg"
-stopwatch.onmouseleave = () => stopwatch.src = "./icons/StopwatchWhiteEmpty.svg"
-
-
-//Code for middle timer circle
+//Middle timer circle variables
 const circle = document.querySelector(".progress");  //Getting top circle
-const text = document.getElementById("timeText");  //Getting middle text
-const radius = circle.r.baseVal.value;  //Getting progress circle radius base value
-const circumference = 2 * Math.PI * radius;  //Calculating circumference of the circle
+const hours = document.querySelector(".hours");  //Getting hours
+const minutes = document.querySelector(".minutes");  //Getting minutes
+const seconds = document.querySelector(".seconds");  //Getting seconds
+const semicolumn1 = document.querySelector(".semicolumn1"); //Getting first semicolumn
+const semicolumn2 = document.querySelector(".semicolumn2"); //Getting second semicolumn
 
-circle.style.strokeDasharray = circumference;  //Making circle with one big dash 
-circle.style.strokeDashoffset = 0;  //Initial offset
-
-let time = 30;
-let current = time, timer;
-
-
-//Function for updateing timer circle
-function updateTimer(){
-    if(current <= 0) {
-      clearInterval(timer); 
-      return;
-    } //If time is 0, stop the timer
-
-    current--;  //Reduce time by one
-    text.textContent = current;  //Updateing middle text
-
-    //Calculating offset (if left time is 40%, offset will be 60%)
-    offset = circumference - (current/time) * circumference; 
-    circle.style.strokeDashoffset = offset;
-
-}
-
-
+//Bottom button variables
 const startBtn = document.querySelector(".startButton");
 const resetBtn = document.querySelector(".resetButton");
 const pauseBtn = document.querySelector(".pauseButton");
 const resumeBtn = document.querySelector(".resumeButton");
 const pauseReset = document.querySelector(".pauseReset");
 
+
+const radius = circle.r.baseVal.value;  //Getting progress circle radius base value
+const circumference = 2 * Math.PI * radius;  //Calculating circumference of the circle
+
+circle.style.strokeDasharray = circumference;  //Making circle with one big dash 
+circle.style.strokeDashoffset = 0;  //Initial offset
+
+let defaultH = 0, defaultMin = 1, defaultSec = 5;
+let currentH = defaultH, currentMin = defaultMin, currentSec = defaultSec;
+let timer, secondsSum = 0, currentSecondsSum = 0; 
+
+//Header stopwatch icon variable
+let stopwatch = document.getElementById("stopwatch");
+
+//Code for stopwatch icon hovering
+stopwatch.onmouseover = () => stopwatch.src = "./icons/StopwatchWhiteFill.svg"
+stopwatch.onmouseleave = () => stopwatch.src = "./icons/StopwatchWhiteEmpty.svg"
+
+
+secondsSum = (((defaultH * 60) + defaultMin) * 60) + defaultSec;  //Starting seconds sum
+
+//Function for updateing timer circle
+function updateTimer(){
+    if(currentH <= 0 && currentMin <= 0 && currentSec <= 0) {
+      clearInterval(timer); 
+      return;
+    } //If time is 0, stop the timer
+
+    if(currentSec > 0){
+      currentSec--;  //Reduce seconds by one
+      displaySeconds();
+    }else if(currentMin > 0){
+      currentMin--;  //Reduce minutes by one
+      displayMinutes();
+      currentSec = 59;
+      seconds.textContent = currentSec;
+    }else if(currentH > 0){
+      currentH--;  //Reduce hours by one
+      displayHours();
+      currentMin = 59;
+      minutes.textContent = currentMin;
+    }
+
+    //Current seconds sum
+    currentSecondsSum = (((currentH * 60) + currentMin) * 60) + currentSec;  
+
+    //Calculating offset (if left time is 40%, offset will be 60%)
+    offset = circumference - (currentSecondsSum/secondsSum) * circumference; 
+    circle.style.strokeDashoffset = offset;
+
+}
+
 //Event handler for starting timer
 function startTimer(){
-    //If timer is not already running, start the timer and run function every second
-    if(current === time) timer = setInterval(updateTimer, 1000);
+    //Start the timer and run function every second
+    timer = setInterval(updateTimer, 1000);
     startBtn.style.display = "none";
     pauseReset.style.display = "flex";
 }
 
 function resetTimer(){
     clearInterval(timer);
-    current = time;
-    text.textContent = current;
+    currentH = defaultH;
+    currentMin = defaultMin;
+    currentSec = defaultSec;
+
+    displayHours();
+    displayMinutes();
+    displaySeconds();
     
     circle.style.transition = "none";
     circle.style.strokeDashoffset = 0;
@@ -74,6 +102,35 @@ function resumeTimer(){
     timer = setInterval(updateTimer, 1000);
     resumeBtn.style.display = "none";
     pauseBtn.style.display = "inline-block";
+}
+
+function displaySeconds(){
+  if(currentSec >= 10) seconds.textContent = currentSec;
+  else seconds.textContent = "0" + currentSec;
+}
+
+function displayMinutes(){
+  if(currentMin === 0) {
+    minutes.style.display = "none";
+    semicolumn2.style.display = "none";
+  }else{
+    minutes.style.display = "block";
+    semicolumn2.style.display = "block";
+    if(currentMin >= 10) minutes.textContent = currentMin;
+    else minutes.textContent = "0" + currentMin;
+  }
+}
+
+function displayHours(){
+  if(currentH === 0) {
+    hours.style.display = "none";
+    semicolumn1.style.display = "none";
+  }else{
+    hours.style.display = "block";
+    semicolumn1.style.display = "block";
+    if(currentH >= 10) hours.textContent = currentH;
+    else hours.textContent = "0" + currentH;
+  }
 }
 
 
--- main.js
+++ main.js
@@ -2,5 +2,6 @@ let sunMoon;
 
 sunMoon = document.getElementById("sunMoon");
 
+//Making hover effect for white mode sun icon
 sunMoon.onmouseover = () => sunMoon.src = "./icons/SunWhiteFill.svg";
 sunMoon.onmouseleave = () => sunMoon.src = "./icons/SunWhiteEmpty.svg";
