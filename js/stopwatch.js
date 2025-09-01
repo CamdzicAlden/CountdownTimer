764738b5db0cf0d673c2d1e083d66225e8a5d868
CamdzicAlden <aldencamdzic3@gmail.com>
2025-09-01 13:09:27 +0200

    feat: Add faveicon to all pages

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,6 +1,8 @@
+//Header icons
 const timer = document.getElementById("timer");
 const sunMoon = document.getElementById("sunMoon");
 
+//Footer buttons
 const startBtn = document.querySelector(".startButton");
 const pauseBtn = document.querySelector(".pauseButton");
 const resetBtn = document.querySelector(".resetButton");
@@ -21,6 +23,7 @@ timer.onmouseleave = () => timer.src = "../icons/TimerWhiteEmpty.svg"
 sunMoon.onmouseover = () => sunMoon.src = "../icons/SunWhiteFill.svg";
 sunMoon.onmouseleave = () => sunMoon.src = "../icons/SunWhiteEmpty.svg";
 
+//Function for updateing stopwatch
 function updateStopwatch(){
     if(currentSec < 59){
         currentSec++;
@@ -36,12 +39,14 @@ function updateStopwatch(){
     }
 }
 
+//Event handler for start button (for starting stopwatch)
 function startStopwatch(){
     stopwatch = setInterval(updateStopwatch, 1000);
     startBtn.style.display = "none";
     pauseReset.style.display = "flex";
 }
 
+//Event handler for reset button
 function resetStopwatch(){
     clearInterval(stopwatch);
     currentH = 0;
@@ -53,32 +58,40 @@ function resetStopwatch(){
     startBtn.style.display = "inline-block";
 }
 
+//Event handler for pause button
 function pauseStopwatch(){
     clearInterval(stopwatch);
     pauseBtn.style.display = "none";
     resumeBtn.style.display = "inline-block";
 }
 
+//Event handler for resume button
 function resumeStopwatch(){
     stopwatch = setInterval(updateStopwatch, 1000);
     pauseDisplay();
 }
 
+//Function for displaying seconds in correct format
 function updateSeconds(){
     if(currentSec < 10) seconds.textContent = "0" + currentSec;
     else seconds.textContent = currentSec;
 }
+
+//Function for displaying minutes and seconds in correct format
 function updateMinutes(){
     if(currentMin < 10) minutes.textContent = "0" + currentMin;
     else minutes.textContent = currentMin;
     updateSeconds();
 }
+
+//Function for displaying whole stopwatch in correct format
 function updateHours(){
     if(currentH < 10) hours.textContent = "0" + currentH;
     else hours.textContent = currentH;
     updateMinutes();
 }
 
+//Function for changing resume button with pause button
 function pauseDisplay(){
   resumeBtn.style.display = "none";
   pauseBtn.style.display = "inline-block";
